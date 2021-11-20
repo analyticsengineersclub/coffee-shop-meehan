bf475a52bf7ea38e983617109654045bffbd1de1
datadisciple <sean@sparkgrills.com>
2021-11-20 20:23:15 +0000

    defines model as incremental

--- models/form_respondents.sql
+++ models/form_respondents.sql
@@ -1,9 +1,18 @@
 {{ config(
-    materialized='table'
+    materialized='incremental'
 ) }}
 
 with events as (
     select * from {{ source('advanced_dbt_examples', 'form_events') }}
+
+    --the below is necessary for incremental models to tell dbt how to delineate what the new records are from the last run
+    {% if is_incremental() %}
+    where timestamp >= (select max(last_form_entry) from {{this}})
+    {% endif %}
+
+    -- where {{this}} represents the currently existing object mapped to this model
+    -- it's important that {{this}} is wrapped in an if statement:
+    -- because if this was the first run of the model it would get materialized as a table and {{this}} doesn't exist yet
 ),
 
 aggregated as (
